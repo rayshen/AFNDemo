08500dc46b87071a92dd49daf7120f1b131a7a56
rayshen <rayshen1012@gmail.com>
2015-06-27 16:21:48 +0800

    Initial Commit

---
+++ AFNDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AFNDemo
+//
+//  Created by shen on 15/6/27.
+//  Copyright (c) 2015年 shen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
